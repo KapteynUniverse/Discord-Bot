02f9efefdee5e894e852b989d576c0648303571a
Asilcan Toper <asilcantoper@gmail.com>
2026-05-08 18:38:50 +0300

    Fixed async function

--- src/commands/events/clientReady.js
+++ src/commands/events/clientReady.js
@@ -2,20 +2,24 @@ import { REST, Routes } from "discord.js";
 
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
-export default function clientReadyHandler(client) {
+export default async function clientReadyHandler(client) {
   console.log(`Logged in as ${client.user.tag}!`);
 
   try {
-  console.log(`Started refreshing ${client.commands.length} commands!`);
+    console.log(`Started refreshing ${client.commands.size} commands!`);
 
-  const data = await rest.put(Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.DISCORD_GUILD_ID), {
-    body: client.commands.map((command) => command.data.toJSON()),
-  });
+    const data = await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.DISCORD_CLIENT_ID,
+        process.env.DISCORD_GUILD_ID,
+      ),
+      {
+        body: client.commands.map((command) => command.data.toJSON()),
+      },
+    );
 
-  console.log(`Successfully reloaded ${data.length} commands.`);
+    console.log(`Successfully reloaded ${data.length} commands.`);
   } catch (error) {
     console.error(error);
   }
-
-
 }
