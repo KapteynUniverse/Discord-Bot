aec7bb6289b054a8e46b703b736ec773bb670f9f
Asilcan Toper <asilcantoper@gmail.com>
2026-05-08 17:39:52 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { Client } from "discord.js";
+
+const client = new Client({});
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
