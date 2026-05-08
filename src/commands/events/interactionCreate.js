7cb8927b097d420acb6abd333e9a3aa21af68a13
Asilcan Toper <asilcantoper@gmail.com>
2026-05-08 18:48:33 +0300

    Added interaction handler

---
+++ src/commands/events/interactionCreate.js
@@ -0,0 +1,15 @@
+export default async function interactionCreateHandler(interaction) {
+  if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
 import "dotenv/config";
 import clientReadyHandler from "./commands/events/clientReady.js";
+import interactionCreateHandler from "./commands/events/interactionCreate.js";
 import * as ping from "./commands/ping.js";
 import express from "express";
 
@@ -18,7 +19,9 @@ app.listen(port, () => {
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
-client.on(Events.ClientReady, clientReadyHandler);
+client.once(Events.ClientReady, clientReadyHandler);
+
+client.on(Events.InteractionCreate, interactionCreateHandler);
 
 client.commands = new Collection();
 
