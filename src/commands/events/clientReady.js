16bf786a0eefa145fefaefacc36324feb47ddd16
Asilcan Toper <asilcantoper@gmail.com>
2026-05-08 18:18:12 +0300

    Added ping pong command

---
+++ src/commands/events/clientReady.js
@@ -0,0 +1,5 @@
+function clientReadyHandler(client) {
+  console.log(`Logged in as ${client.user.tag}!`);
+}
+
+export default clientReadyHandler;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import "dotenv/config";
+import clientReadyHandler from "./commands/events/clientReady";
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
-client.on(Events.ClientReady, () => {
-  console.log(`Logged in!`);
-});
+client.on(Events.ClientReady, clientReadyHandler);
 
 client.login(process.env.DISCORD_TOKEN);
