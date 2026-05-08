5c0c5aab253eca5fa09277520c69e7354e6766ad
Asilcan Toper <asilcantoper@gmail.com>
2026-05-08 19:41:17 +0300

    Added forecast command

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import clientReadyHandler from "./events/clientReady.js";
 import interactionCreateHandler from "./events/interactionCreate.js";
 import * as ping from "./commands/ping.js";
+import * as forecast from "./commands/forecast.js";
 import express from "express";
 
 const app = express();
@@ -23,6 +24,7 @@ const client = new Client({
 client.commands = new Collection();
 
 client.commands.set(ping.data.name, ping);
+client.commands.set(forecast.data.name, forecast);
 
 client.once(Events.ClientReady, clientReadyHandler);
 client.on(Events.InteractionCreate, interactionCreateHandler);
